9952f78b24e2486112a290100938723e5da6e88e
wl <kcwl558106@gmail.com>
2021-06-22 00:41:49 +0800

    c5-1定義一個匿名函數

--- SimVillage.kt
+++ SimVillage.kt
@@ -1,6 +1,8 @@
+/*c5-1 定義一個匿名函數
+*/
 fun main(){
-    val numLetters = "Mississippi".count{
-        letter -> letter == 's'
-    }
-    print(numLetters)
+    println({
+        val currentYear = 2021
+        "歡迎蒞臨 SimVillage，鎮長！ (copyright $currentYear)"
+    }())
 }
