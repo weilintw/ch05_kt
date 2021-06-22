63aaee9835904b57c5a347cc591d37a8d29d78fe
wl <kcwl558106@gmail.com>
2021-06-22 15:49:24 +0800

    c5-13增加configuraGreetingFunction函數

--- SimVillage.kt
+++ SimVillage.kt
@@ -1,20 +1,20 @@
-/*c5-12 傳遞 printConstructionCost 函數參照
+/*c5-13 增加configuraGreetingFunction函數
 */
 fun main(){
-    runSimulation("wl",::printConstructionCost){
-        playerName, numBuildings ->
+    runSimulation()
+}
+inline fun runSimulation(){
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("wl"))
+}
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "醫院"
+    var numBuildings = 5
+    return {
+        playerName: String ->
         val currentYear = 2021
-        println("新建 $numBuildings 棟房屋")
+        numBuildings += 1
+        println("新建 $numBuildings 間 $structureType")
         "歡迎蒞臨 SimVillage，$playerName！ (copyright $currentYear)"
     }
-}
-inline fun runSimulation(playerName: String,
-                         costPrinter: (Int) -> Unit, greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..3).shuffled().last()  //亂數選擇1,2或3
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
-}
-fun printConstructionCost(numBuildings: Int){
-    val cost = 500
-    println("建造成本：${cost * numBuildings}")
 }
