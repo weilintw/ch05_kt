4656df833563c411f40838b99baddb7b6c2f6131
wl <kcwl558106@gmail.com>
2021-06-22 14:41:53 +0800

    c5-8使用簡略語法傳入lambda引數

--- SimVillage.kt
+++ SimVillage.kt
@@ -1,13 +1,12 @@
-/*c5-7 新增runSimulation函數
+/*c5-8 使用簡略語法傳入lambda引數
 */
 fun main(){
-    val greetingFunction = {
-        playerName: String,numBuildings: Int ->
+    runSimulation("wl"){
+        playerName, numBuildings ->
         val currentYear = 2021
         println("新建 $numBuildings 棟房屋")
         "歡迎蒞臨 SimVillage，$playerName！ (copyright $currentYear)"
     }
-    runSimulation("wl",greetingFunction )
 }
 fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
     val numBuildings = (1..3).shuffled().last()  //亂數選擇1.2或3
