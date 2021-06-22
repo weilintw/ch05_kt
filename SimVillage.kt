ded7ba5004fea4cc26210c1a571a4fc8451482ef
wl <kcwl558106@gmail.com>
2021-06-22 15:18:02 +0800

    c5-10定義printConstructionCost函數

--- SimVillage.kt
+++ SimVillage.kt
@@ -1,4 +1,4 @@
-/*c5-9 使用inline
+/*c5-10 定義printConstructionCost函數
 */
 fun main(){
     runSimulation("wl"){
@@ -9,6 +9,10 @@ fun main(){
     }
 }
 inline fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..3).shuffled().last()  //亂數選擇1.2或3
+    val numBuildings = (1..3).shuffled().last()  //亂數選擇1,2或3
     println(greetingFunction(playerName, numBuildings))
+}
+fun printConstructionCost(numBuildings: Int){
+    val cost = 500
+    println("建造成本： ${cost * numBuildings}")
 }
