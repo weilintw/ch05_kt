bbb6ac4f976eb3f2b50300a9b63a34f83ff87377
wl <kcwl558106@gmail.com>
2021-06-22 13:01:51 +0800

    c5-7新增runSimulation函數

--- SimVillage.kt
+++ SimVillage.kt
@@ -1,4 +1,4 @@
-/*c5-6 使用類型推斷
+/*c5-7 新增runSimulation函數
 */
 fun main(){
     val greetingFunction = {
@@ -7,5 +7,9 @@ fun main(){
         println("新建 $numBuildings 棟房屋")
         "歡迎蒞臨 SimVillage，$playerName！ (copyright $currentYear)"
     }
-    println(greetingFunction("wl",2))
+    runSimulation("wl",greetingFunction )
+}
+fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+    val numBuildings = (1..3).shuffled().last()  //亂數選擇1.2或3
+    println(greetingFunction(playerName, numBuildings))
 }
